258519385efb7b7a2f091f51173988dc2a8dad96
intancious <intancious98@gmail.com>
2021-05-25 10:14:28 +0700

    ubahh" title

--- application/controllers/admin/Slider.php
+++ application/controllers/admin/Slider.php
@@ -5,7 +5,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Slider extends CI_Controller
 {
 
-    private $title = "Title";
+    private $title = "Slider";
 
     public function __construct()
     {
