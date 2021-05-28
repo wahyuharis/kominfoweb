f3529051c9bb4151a76bea7e3830cd994fab08b6
harits wahyu <harits.wahyu.hid@gmail.com>
2021-05-28 07:26:00 +0700

    css image

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,8 +4,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-
-
     public function __construct()
     {
         parent::__construct();
@@ -13,7 +11,12 @@ class Home extends CI_Controller
 
     public function index()
     {
+        $view_data['slider']=$this->db->get('sliders')->result_array();
 
-        echo "home front";
+        // print_r2($view_data['slider'] );
+
+        $this->load->view('frontend/template',$view_data);
     }
+
+
 }
