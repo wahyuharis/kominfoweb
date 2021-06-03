37c5d912eb8ceb2d96576ea8ba3fd9327b52b1bb
intancious <intancious98@gmail.com>
2021-06-03 12:43:57 +0700

    menambah tabel regulasi, controller bagian profil dan view

---
+++ application/controllers/admin/Sejarah_Dinas.php
@@ -0,0 +1,53 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sejarah_Dinas extends CI_Controller
+{
+
+    private $title = "Profil Sejarah Dinas";
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('grocery_CRUD');
+        $this->load->library('Auth');
+        $auth = new Auth();
+        $auth->is_logged_in();
+    }
+
+    public function index()
+    {
+
+        $crud = new grocery_CRUD();
+        $crud->unset_bootstrap(); /*wajib ada karena boostrap grocery bentrok dengan boodtrap adminlte*/
+        $crud->unset_jquery(); /*wajib ada karena boostrap grocery bentrok dengan jquery adminlte*/
+
+        $crud->set_theme('bootstrap');
+        $crud->set_table('profile_sejarah_dinas');
+        $crud->required_fields('kategori', 'judul', 'konten', 'gambar');
+
+        $crud->columns('kategori', 'judul', 'konten', 'gambar');
+
+        $crud->display_as('kategori', 'Kategori');
+        $crud->display_as('judul', 'Judul');
+        $crud->display_as('konten', 'Konten');
+        $crud->display_as('gambar', 'Gambar');
+
+        $crud->required_fields('kategori', 'judul', 'konten', 'gambar');
+
+        $crud->set_field_upload('gambar', 'assets/uploads/files');
+
+
+        $crud->set_subject('Profil Sejarah Dinas');
+
+        $output = $crud->render();
+
+        $template_data['content'] = $output->output;
+        $template_data['content_title'] = $this->title;
+        $template_data['js_files'] = $output->js_files;
+        $template_data['css_files'] = $output->css_files;
+
+        $this->load->view('admin/template', $template_data);
+    }
+}
