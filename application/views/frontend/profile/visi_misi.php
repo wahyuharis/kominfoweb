4ebee2da8cf7c411e7e3b983f5f4d4f5fb37edb2
intancious <intancious98@gmail.com>
2021-06-07 09:20:55 +0700

    controller dan view profil visi misi

--- application/views/frontend/profile/visi_misi.php
+++ application/views/frontend/profile/visi_misi.php
@@ -1,87 +1,59 @@
-<!--================Blog Area =================-->
 <section class="blog_area single-post-area section-padding">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 posts-list">
                 <div class="single-post">
                     <div class="feature-img">
-                        <img class=<?= base_url('assets/uploads/files/' . $visi_misi->gambar) ?> alt="">
+                        <img class="img-fluid" src="<?= base_url('assets/uploads/files/' . $visi_misi->gambar) ?>" alt="">
                     </div>
                     <div class="blog_details">
-                        <h2><?= $visi_misi->judul ?></h2>
-                        <ul class="blog-info-link mt-3 mb-4">
-                            <!-- <li><a href="#"><i class="fa fa-user"></i> <?= $visi_misi->fullname ?></a></li> -->
-                            <!-- <li><a href="#"><i class="fa fa-calendar"></i> <?= waktu_ymd_to_dmy($visi_misi->date)  ?></a></li> -->
+                        <h2>
+                            <?= $visi_misi->judul ?>
+                        </h2>
+                        <br>
+
+                        <!-- <ul class="blog-info-link mt-3 mb-4">
+                            <li><a href="#"><i class="fa fa-user"></i> <?= $berita_detail->fullname ?></a></li>
+                            <li><a href="#"><i class="fa fa-calendar"></i> <?= waktu_ymd_to_dmy($berita_detail->date)  ?></a></li> -->
                         </ul>
-                        <p class="excert">
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                            should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                            fraction of the camp price. However, who has the willpower
-                        </p>
-                        <p>
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                            should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                            fraction of the camp price. However, who has the willpower to actually sit through a
-                            self-imposed MCSE training. who has the willpower to actually
-                        </p>
-                        <div class="quote-wrapper">
-                            <div class="quotes">
-                                MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                                should have to spend money on boot camp when you can get the MCSE study materials yourself at
-                                a fraction of the camp price. However, who has the willpower to actually sit through a
-                                self-imposed MCSE training.
-                            </div>
-                        </div>
-                        <p>
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                            should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                            fraction of the camp price. However, who has the willpower
-                        </p>
-                        <p>
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                            should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                            fraction of the camp price. However, who has the willpower to actually sit through a
-                            self-imposed MCSE training. who has the willpower to actually
-                        </p>
+                        <br>
+
+                        <?= $visi_misi->konten ?>
+
+
                     </div>
                 </div>
                 <div class="navigation-top">
                     <div class="d-sm-flex justify-content-between text-center">
-                        <p class="like-info"><span class="align-middle"><i class="fa fa-heart"></i></span> Lily and 4
-                            people like this</p>
+                        <p class="like-info"><span class="align-middle"><i class="fa fa-eye"></i></span>
+                            <?= $visi_misi->judul ?>
+                            kali dilihat </p>
                         <div class="col-sm-4 text-center my-2 my-sm-0">
                             <!-- <p class="comment-count"><span class="align-middle"><i class="fa fa-comment"></i></span> 06 Comments</p> -->
                         </div>
                         <ul class="social-icons">
                             <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                             <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fab fa-behance"></i></a></li>
                         </ul>
                     </div>
+
                     <div class="navigation-area">
                         <div class="row">
-                            <div class="col-lg-6 col-md-6 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
 
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-12 nav-right flex-row d-flex justify-content-end align-items-center">
-
-                            </div>
                         </div>
                     </div>
                 </div>
-
-
+                <!--kalau dihapus berita kanan pindah bawah-->
             </div>
             <div class="col-lg-4">
                 <div class="blog_right_sidebar">
                     <aside class="single_sidebar_widget search_widget">
-                        <form action="#">
+                        <form action="<?= base_url('blog') ?>" method="get">
                             <div class="form-group">
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder='Search Keyword' onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Keyword'">
+                                    <input type="text" name="search" class="form-control" placeholder='Search Keyword' onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Keyword'">
                                     <div class="input-group-append">
-                                        <button class="btns" type="button"><i class="ti-search"></i></button>
+                                        <button class="btns" type="submit"><i class="ti-search"></i></button>
                                     </div>
                                 </div>
                             </div>
@@ -89,86 +61,44 @@
                         </form>
                     </aside>
 
+
                     <aside class="single_sidebar_widget popular_post_widget">
-                        <h3 class="widget_title">Recent Post</h3>
-                        <div class="media post_item">
-                            <img src="assets/img/post/post_1.png" alt="post">
-                            <div class="media-body">
-                                <a href="single-blog.html">
-                                    <h3>From life was you fish...</h3>
-                                </a>
-                                <p>January 12, 2019</p>
-                            </div>
-                        </div>
-                        <div class="media post_item">
-                            <img src="assets/img/post/post_2.png" alt="post">
-                            <div class="media-body">
-                                <a href="single-blog.html">
-                                    <h3>The Amazing Hubble</h3>
-                                </a>
-                                <p>02 Hours ago</p>
-                            </div>
-                        </div>
-                        <div class="media post_item">
-                            <img src="assets/img/post/post_3.png" alt="post">
-                            <div class="media-body">
-                                <a href="single-blog.html">
-                                    <h3>Astronomy Or Astrology</h3>
-                                </a>
-                                <p>03 Hours ago</p>
-                            </div>
-                        </div>
-                        <div class="media post_item">
-                            <img src="assets/img/post/post_4.png" alt="post">
-                            <div class="media-body">
-                                <a href="single-blog.html">
-                                    <h3>Asteroids telescope</h3>
-                                </a>
-                                <p>01 Hours ago</p>
-                            </div>
-                        </div>
+                        <h3 class="widget_title">Berita Terbaru</h3>
+                        <ul class="data-list" data-autoscroll>
+                            <?php foreach ($berita_kanan as $bkanan) : ?>
+                                <div class="media post_item">
+                                    <img src="<?= base_url('assets/uploads/files/' . $bkanan['image']) ?>" alt="post" width="65px" height="65px" style="border-radius: 5px;">
+                                    <div class="media-body hover-show">
+                                        <span class="color2">See beach</span>
+                                        <a href="<?= base_url('blog/detail/' . $bkanan['slug']) ?>">
+                                            <h3 class="short-title"><?= substr($bkanan['title'], 0, 50) ?>...</h3>
+                                            <h3 class="long-title"><?= ($bkanan['title']) ?></h3>
+                                        </a>
+                                        <p><?= waktu_ymd_to_dmy($bkanan['date']) ?></p>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        </ul>
+
                     </aside>
 
                     <aside class="single_sidebar_widget instagram_feeds">
                         <h4 class="widget_title">Instagram Feeds</h4>
-                        <ul class="instagram_row flex-wrap">
-                            <li>
-                                <a href="#">
-                                    <img class="img-fluid" src="assets/img/post/post_5.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img class="img-fluid" src="assets/img/post/post_6.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img class="img-fluid" src="assets/img/post/post_7.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img class="img-fluid" src="assets/img/post/post_8.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img class="img-fluid" src="assets/img/post/post_9.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img class="img-fluid" src="assets/img/post/post_10.png" alt="">
-                                </a>
-                            </li>
-                        </ul>
+                        <iframe src="https://snapwidget.com/embed/940247" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:100%; height:510px"></iframe>
                     </aside>
-
 
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!--================ Blog Area end =================-->
+<script>
+    $(document).ready(function() {
+        $('.blog_details').find('p').css('font-family', '"Roboto", sans-serif');
+        $('.blog_details').find('span').css('font-family', '"Roboto", sans-serif');
+        $('.blog_details').find('table').addClass('table');
+        $('.blog_details').find('table').addClass('table-bordered');
+        $('.blog_details').find('table').removeAttr('border');
+
+    });
+</script>
