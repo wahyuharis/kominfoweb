fa13933ec2d42e1c5abfdaa2edc6c3a93b53833b
rosidf26 <frosid26@gmail.com>
2021-06-29 08:36:27 +0700

    edit caption search sidebar kanan

--- application/views/frontend/profile/visi_misi.php
+++ application/views/frontend/profile/visi_misi.php
@@ -48,13 +48,13 @@
                         <form action="<?= base_url('blog') ?>" method="get">
                             <div class="form-group">
                                 <div class="input-group mb-3">
-                                    <input type="text" name="search" class="form-control" placeholder='Search Keyword' onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Keyword'">
+                                    <input type="text" name="search" class="form-control" placeholder='Cari Berita' onfocus="this.placeholder = ''" onblur="this.placeholder = 'Cari Berita'">
                                     <div class="input-group-append">
-                                        <button class="btns" type="submit"><i class="ti-search"></i></button>
+                                        <button class="btns" type="reset" title="Reset"><i class="ti-close"></i></button>
                                     </div>
                                 </div>
                             </div>
-                            <button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn" type="submit">Search</button>
+                            <button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn" type="submit">Cari</button>
                         </form>
                     </aside>
 
