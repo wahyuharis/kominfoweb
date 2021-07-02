bd51ff666caace72a201be8a5095e446c125381a
harits wahyu <harits.wahyu.hid@gmail.com>
2021-07-02 14:51:05 +0700

    zoom js tested

--- template_kominfo/assets/js/zoom.js
+++ template_kominfo/assets/js/zoom.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     var width=$( window ).width();
     console.log(width);
 
-    if( width >= 1920  &&  width < 2560 ){
+    if( width >= 1900  &&  width < 2500 ){
         document.body.style.zoom = "150%";
     }
 
-    if( width >= 2560  ){
+    if( width >= 2500  ){
         document.body.style.zoom = "170%";
         
     }
